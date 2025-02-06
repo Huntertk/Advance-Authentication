f739d41f9eb91ead3c8c3e752a5ba5837b67ce24
Huntertk <taufik.khan65@gmail.com>
2025-02-06 22:26:03 +0530

    login endpoint and service is created

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,18 +1,9 @@
-import { z } from "zod";
 import catchErrors from "../utils/catchErrors";
-import { createAccount } from "../services/auth.service";
-import { CREATED } from "../constants/http";
+import { createAccount, loginUser } from "../services/auth.service";
+import { CREATED, OK } from "../constants/http";
 import { setAuthCookies } from "../utils/cookies";
-
-const registerSchema = z.object({
-    email:z.string().email().min(1).max(255),
-    password:z.string().min(6).max(255),
-    confirmPassword:z.string().min(6).max(255),
-    userAgent:z.string().optional(),
-}).refine((data) => data.password === data.confirmPassword, {
-    message:"Password does not match",
-    path:["confirmPassword"]
-});
+import { loginSchema, registerSchema } from "./auth.schemas";
+import { allowedNodeEnvironmentFlags } from "process";
 
 
 export const registerHandler = catchErrors (
@@ -27,5 +18,16 @@ export const registerHandler = catchErrors (
 
         //return response
         return setAuthCookies({res, accessToken, refreshToken}).status(CREATED).json(user)
+    }
+)
+
+export const loginHandler = catchErrors(
+    async (req, res, next) => {
+        const request = loginSchema.parse({...req.body, userAgent:req.headers["user-agent"]}); 
+        const {accessToken, refreshToken, user} = await loginUser(request);
+       //return response
+       return setAuthCookies({res, accessToken, refreshToken}).status(OK).json({
+        message:"Login Successfully"
+       })
     }
 )
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
-import { CONFLICT } from "../constants/http";
+import { CONFLICT, UNAUTHORIZED } from "../constants/http";
 import VerificationCodeType from "../constants/verificationCodeType";
 import SessionModel from "../models/session.model";
 import UserModel from "../models/user.model";
@@ -65,4 +65,51 @@ export const createAccount = async (data:CreateAccountParams) => {
 
     //return new user and access token 
     return {user:user.omitPassword(), accessToken, refreshToken}
+}
+
+export type LoginAccountParams = {
+    email:string;
+    password:string;
+    userAgent?:string;
+}
+
+export const loginUser = async ({email, password, userAgent}:LoginAccountParams) => {
+    // get the user the by email
+    const user  = await UserModel.findOne({email});
+    appAssert(user, UNAUTHORIZED, "Invalid email and password");
+
+    //validate the password from the request
+    const isValid = await user.comparePassword(password);
+    appAssert(isValid, UNAUTHORIZED, "Invalid email and password");
+    
+    //create session
+    const userId = user._id;
+    const session = await SessionModel.create({userId, userAgent});
+    const sessionInfo = {
+        sessionId:session._id
+    }
+    
+    //sign access token and refresh token
+    const refreshToken = jwt.sign(
+        sessionInfo,
+        JWT_REFRESH_SECRET,
+        {
+            expiresIn:"30d",
+            audience:["user"]
+        }
+    )
+    const accessToken = jwt.sign(
+        {
+            ...sessionInfo,
+            userId:user._id
+        },
+        JWT_SECRET,
+        {
+            expiresIn:"15m",
+            audience:["user"]
+        }
+    )
+
+    // return user and token
+    return {user:user.omitPassword(),accessToken, refreshToken}
 }
